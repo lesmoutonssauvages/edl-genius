6fdc7bd6c90f0a494ae383a9e05682a6d6b5805e
Charles <charles@lesmoutonssauvages.com>
2025-04-06 08:39:16 +0200

    fix(tscofig): add output dir to exclude

--- dist/lib/EditDecisionList.d.ts
+++ dist/lib/EditDecisionList.d.ts
@@ -1,5 +1,3 @@
-/// <reference types="node" />
-/// <reference types="node" />
 import { Readable } from 'stream';
 import Event, { EventAttributes } from './Event.js';
 export interface EditDecisionListAttributes {
--- dist/lib/EditDecisionList.js
+++ dist/lib/EditDecisionList.js
@@ -9,8 +9,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
+    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
@@ -102,10 +102,10 @@ var EditDecisionList = (function () {
             });
         });
     };
-    EditDecisionList.prototype.readBuffer = function (buf, encoding) {
-        if (encoding === void 0) { encoding = 'utf8'; }
-        return __awaiter(this, void 0, void 0, function () {
+    EditDecisionList.prototype.readBuffer = function (buf_1) {
+        return __awaiter(this, arguments, void 0, function (buf, encoding) {
             var stream;
+            if (encoding === void 0) { encoding = 'utf8'; }
             return __generator(this, function (_a) {
                 stream = getBasicStream();
                 stream.push(buf, encoding);
